19fc512d93e97448407cd49dd7958840f650ab25
yoonsen <yoonsen@gmail.com>
2022-10-25 18:09:31 +0200

    updated files

--- themes.py
+++ themes.py
@@ -1,49 +1,17 @@
 import streamlit as st
 import dhlab.text as dh
 import dhlab.api.dhlab_api as api
-import gnl as gnl
+import gnl
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 from PIL import Image
 import requests
 from streamlit_agraph import agraph, TripleStore, Config, Node, Edge
+import re
 
 maxlen = 250
 antall_dokument = 30
-
-colors =  ['#DC143C','#FFA500',
-           '#F0E68C','#BC8F8F','#32CD32',
-           '#D2691E','#3CB371','#00CED1',
-           '#00BFFF','#8B008B','#FFC0CB',
-           '#FF00FF','#FAEBD7']
-
-def word_to_colors(comm):
-    word_to_color = dict()
-    for i, e in enumerate(comm.values()):
-        for x in e:
-            word_to_color[x] = colors[i % len(colors)]
-    return word_to_color
-
-
-@st.cache(suppress_st_warning=True, show_spinner = False)
-def create_nodes_and_edges_config(g, community_dict):
-    """create nodes and edges from a networkx graph for streamlit agraph, classes Nodes, Edges and Config must be imported"""
-    cmap = word_to_colors(community_dict)
-    nodes = []
-    edges = []
-    for i in g.nodes(data = True):
-        nodes.append(Node(id=i[0], size=100, color=cmap[i[0]]) )
-    for i in g.edges(data = True):
-        edges.append(Edge(source=i[0], target=i[1], type="CURVE_SMOOTH", color = "#ADD8E6"))
-
-    config = Config(height=500,
-                nodeHighlightBehavior=True,
-                highlightColor="#F7A7A6", 
-                directed=True, 
-                collapsible=True)
-    
-    return nodes, edges, config
 
 
 @st.cache(suppress_st_warning=True, show_spinner = False)
@@ -75,13 +43,16 @@ def theme_book_list(urn = None, words = None, chunksize= 1000):
 
 
 @st.cache(suppress_st_warning=True, show_spinner = False)
-def get_corpus(freetext=None, title=None, from_year=1900, to_year=2020, number = 20):
-    c = dh.Corpus(freetext=freetext, title=title,from_year=from_year, to_year=to_year, limit = number)
+def get_corpus(freetext=None, title=None, number = 20):
+    if not freetext is None:
+        c = dh.Corpus(freetext=freetext, title=title, limit = number)
+    else:
+        c = dh.Corpus(doctype="digibok", limit = number)
     return c.corpus
 
 @st.cache(suppress_st_warning=True, show_spinner = False)
-def totals(n=400000):
-    return api.totals(n=n)
+def totals(n = 300000):
+    return api.totals(n)
 
 
 image = Image.open("DHlab_logo_web_en_black.png")
@@ -97,10 +68,17 @@ st.title('Temaer i tekst')
 # select URN
 # Using the "with" syntax
 
-stikkord = st.text_input('Angi noen stikkord for å forme et utvalg tekster, og velg fra listen under for å se en avis, skriv navnet på avisen eventuelt sammen med årstall')
+stikkord = st.text_input('Angi noen stikkord for å forme et utvalg tekster, og velg fra listen under. For å se en avis, skriv navnet på avisen eventuelt sammen med årstall, eller bare lim inn en URN')
+
+
+urn = []
 if stikkord == '':
     stikkord = None
-
+else:
+    urn = re.findall("URN:NBN[^ ]+", stikkord)
+    if urn != []:
+        urn = urn[0]
+        
  #, from_year=period[0], to_year=period[1])
 
 relevance_list = st.checkbox("Bruk automatisk ordliste", value = True, help="klikk for å legge inn egen kommaseparert ordlist")
@@ -114,10 +92,12 @@ if relevance_list:
 
             #antall_dokument = st.number_input("Antall dokument fra 10 til 100", 
             #                                  min_value = 10, max_value = 100, value = 20)
-            corpus = get_corpus(freetext=stikkord, number = antall_dokument)
-            choices = [', '.join([str(z) for z in x]) for x in corpus[['authors','title', 'year','urn']].values.tolist()]
-            valg = st.selectbox("Velg et dokument", choices)
-            urn = valg.split(', ')[-1]
+            if urn == []:
+                corpus = get_corpus(freetext=stikkord, number = antall_dokument)
+                choices = [', '.join([str(z) for z in x]) for x in corpus[['authors','title', 'year','urn']].values.tolist()]
+                valg = st.selectbox("Velg et dokument", choices)
+                urn = valg.split(', ')[-1]
+                
             submit_button = st.form_submit_button(label='Finn tema!')
 
             #create graph and themes
@@ -128,9 +108,10 @@ if relevance_list:
                 comm =  gnl.community_dict(G)
             except:
                 comm = dict()
-                
-    st.markdown("## Temaer")
-    st.write('\n\n'.join(['**{label}** {value}'.format(label = key, value = ', '.join(comm[key])) for key in comm]))
+            
+            if submit_button:
+                st.markdown("## Temaer")
+                st.write('\n\n'.join(['**{label}** {value}'.format(label = key, value = ', '.join(comm[key])) for key in comm]))
 
 else:
     with st.form(key='my_form'):
@@ -157,16 +138,9 @@ else:
                 comm =  gnl.community_dict(G)
             except:
                 comm = dict()
-            
-    st.markdown("## Temaer")
-    st.write('\n\n'.join(['**{label}** {value}'.format(label = key, value = ', '.join(comm[key])) for key in comm]))
+            if submit_button:
+                st.markdown("## Temaer")
+                st.write('\n\n'.join(['**{label}** {value}'.format(label = key, value = ', '.join(comm[key])) for key in comm]))
 
-#nodes, edges, config = create_nodes_and_edges_config(G, comm)
 
-# show graph
-#st.markdown("## Graph")
-#agraph(nodes, edges, config)
-
-# show themes
-#------------------------------------------ Clustre -------------------------------###
 
